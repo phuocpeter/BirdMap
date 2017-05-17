d2209a965406d3279c274d411a9b3762f44c7d04
phuocpeter19 <phuocpeter19@gmail.com>
2017-05-17 10:35:10 -0400

    Initial Commit

---
+++ BirdMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BirdMap
+//
+//  Created by Phuoc Tran on 2017-05-16.
+//  Copyright © 2017 Phuoc Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-BirdMap/Pods-BirdMap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BirdMap : NSObject
+@end
+@implementation PodsDummy_Pods_BirdMap
+@end
